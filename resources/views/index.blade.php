a5154497e88f4b880599645d8777ec21f14b8a31
Nguyễn Trọng Giáp <nguyentronggiap97@gmail.com>
2019-08-09 20:22:42 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,90 +6,244 @@
             print_r("ahih");
         echo '</pre>';
     @endphp
-    <div class="ahihi">
-        <div class="container">
-            <div class="row data-in">
-                <div class="col-lg-5 box-data">
-                    <div>
-                        <ul>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                            <li>
-                                <div class="left">Ngứa</div>
-                                <div class="right"><a href="#">Chỉnh sửa</a></div>
-                                <div class="clear"></div>
-                            </li>
-                        </ul>
+        <div class="ahihi">
+            <div class="container">
+                <div class="row data-in">
+                    <div class="col-lg-5 box-data">
+                        <div>
+                            <ul>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                                <li>
+                                    <div class="left">Ngứa</div>
+                                    <div class="right">
+                                        <div>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#modal-edit"  href="#">Chỉnh sửa</a>
+                                            <a class="btn btn-primary" href="#">Xóa</a>
+                                        </div>
+                                    </div>
+                                    <div class="clear"></div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2"></div>
+                    <div class="col-lg-5 box-data box-data-2"></div>
+                </div>
+                <div class="row button-manager">
+                    <div class="col-lg-6"><button class="btn btn-primary" data-toggle="modal" data-target="#modal-add">Thêm Luật</button></div>
+                    <div class="col-lg-6"><button class="btn btn-primary" data-toggle="modal" data-target="#modal-process">Chuẩn đoán</button></div>
+                </div>
+                <div class="row data-process">
+                    
+                </div>
+
+            </div>
+        </div>
+        <div class="modal" id="modal-add">
+            <div class="modal-dialog">
+                <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Modal Add</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <div class="row">
+                        <select class="selectpicker col-md-12 select-add" multiple data-live-search="true">
+                            <option>Mustard</option>
+                            <option>Ketchup</option>
+                            <option>Relish</option>
+                        </select>
+                        <input type="text" name="tile" class="title-add form-control col-md-12">
                     </div>
                 </div>
-                <div class="col-lg-2"></div>
-                <div class="col-lg-5 box-data box-data-2"></div>
-            </div>
-            <div class="row button-manager">
-                
-            </div>
-            <div class="row data-process">
-                
-            </div>
 
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button class="save-add btn-info">Thêm</button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">đóng</button>
+                </div>
+
+                </div>
+            </div>
         </div>
-    </div>
+
+        <div class="modal" id="modal-process">
+            <div class="modal-dialog">
+                <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Modal Process</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <div class="row">
+                        <select class="selectpicker col-md-12 select-process" multiple data-live-search="true">
+                            <option>Mustard</option>
+                            <option>Ketchup</option>
+                            <option>Relish</option>
+                        </select>
+                        <input type="text" name="" class="title-process form-control col-md-12">
+                    </div>
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button class="save-process btn-info">Xử lý</button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">đóng</button>
+                </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="modal" id="modal-edit">
+            <div class="modal-dialog">
+                <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Modal Edit</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+                    Modal body..
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+
+                </div>
+            </div>
+        </div>
+
 @endsection
